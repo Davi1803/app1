978937bbf89fd445b3bc1fa42bfd63bb148e3f1f
20201tiimi0047 <sallescardosodavi@gmail.com>
2023-03-15 09:08:14 -0300

    Comentando o código

--- app/src/main/java/salles/cardoso/davi/app1/MainActivity.java
+++ app/src/main/java/salles/cardoso/davi/app1/MainActivity.java
@@ -15,15 +15,23 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); /*cria a tela*/
+        //cria a tela
+        setContentView(R.layout.activity_main);
+        //criando botão enviar utilizando o id do botão na tela
         Button btnEnviar = findViewById(R.id.btnEnviar);
+        //chamando função ao clicar no botão
         btnEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // pegando o editText da tela utilizando o id
                 EditText etDigiteAqui = findViewById(R.id.etDigiteAqui);
+                //salvando conteúdo do editText em uma variável
                 String textoDigitado = etDigiteAqui.getText().toString();
+                //Pegando os elementos de uma tela (MainActivity) e adicionando na outra (NextActivity)
                 Intent i = new Intent(MainActivity.this,NextActivity.class);
+                //criando um dicionário em que a chave é a palavra Texto e o conteúdo é o que foi digitado anteriormente
                 i.putExtra("Texto", textoDigitado);
+                //iniciando segunda tela
                 startActivity(i);
 
             }
--- app/src/main/java/salles/cardoso/davi/app1/NextActivity.java
+++ app/src/main/java/salles/cardoso/davi/app1/NextActivity.java
@@ -12,9 +12,13 @@ public class NextActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
+        //pegando o intent criador da tela NextActivity
         Intent i = getIntent();
+        //pegando o conteúdo do dicionário criado no MainActivity e salvando em uma String
         String textoDigitado = i.getStringExtra( "Texto");
+        //pegando o TextView na tela utilizando o id
         TextView tvTexto = findViewById(R.id.tvTexto);
+        //adicionando o texto digitado na String (textoDigitado) no textView (tvTexto)
         tvTexto.setText(textoDigitado);
     }
 }
